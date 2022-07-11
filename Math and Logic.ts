a9c4272bf38fe8f7aea51b6429d24337911279ec
AnatoliBel <lisoidkotov@gmail.com>
2022-07-11 19:06:23 +0300

    lesson2

---
+++ Math and Logic.ts
@@ -0,0 +1,89 @@
+//addition
+//string+boolean
+let a1 = 'Hare';
+let b1 = false;
+console.log(typeof a1)
+console.log(typeof b1)
+let c1 = a1 + b1;
+console.log(c1);
+
+//string+number
+let a2 = 'Krishna';
+let b2 = 5;
+console.log(typeof a2)
+console.log(typeof b2)
+let c2 = a2 + b2;
+console.log(c2);
+
+//number+boolean
+let a3 = 6;
+let b3 = true;
+console.log(typeof a3)
+console.log(typeof b3)
+let c3 = a3 + b3;
+console.log(c3);
+
+//multiplication
+//string*boolean
+let a4 = 'Hare';
+let b4 = true;
+console.log(typeof a4)
+console.log(typeof b4)
+let c4 = a4 * b4;
+console.log(c4);
+
+//string*namber
+let a5 = 'Krishna';
+let b5 = 5;
+console.log(typeof a5)
+console.log(typeof b5)
+let c5 = a5 * b5;
+console.log(c5);
+
+//number*boolean
+let a6 = 6;
+let b6 = false;
+console.log(typeof a6)
+console.log(typeof b6)
+let c6 = a6 * b6;
+console.log(c6);
+
+//divide
+//string/boolean
+let a7 = 'Hare';
+let b7 = false;
+console.log(typeof a7)
+console.log(typeof b7)
+let c7 = a7 / b7;
+console.log(c7);
+
+//string/namber
+let a8 = 'Krishna';
+let b8 = 5;
+console.log(typeof a8)
+console.log(typeof b8)
+let c8 = a8 / b8;
+console.log(c8);
+
+//number/boolean
+let a9 = 6;
+let b9 = false;
+console.log(typeof a9)
+console.log(typeof b9)
+let c9 = a9 / b9;
+console.log(c9);
+
+//explicit conversion
+let a10 = "123";
+let b10 = Number (a10);
+let c10 = Boolean (a10)
+console.log(typeof a10)
+console.log(typeof b10)
+console.log(typeof c10)
+let d10 = 1 + b10;
+console.log(d10);
+let e10 = b10 + a10;
+console.log(e10);
+let f10 = c10 + b10;
+console.log(f10);
+
